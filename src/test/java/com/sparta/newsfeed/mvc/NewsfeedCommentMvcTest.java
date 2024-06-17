8b8f88f070a80999c569de5a9545a6f65ecd2c59
Jaeseok Lee <wotjr4017@gmail.com>
2024-06-17 11:16:15 +0900

    Controller 테스트 - 게시물,댓글

--- src/test/java/com/sparta/newsfeed/mvc/NewsfeedCommentMvcTest.java
+++ src/test/java/com/sparta/newsfeed/mvc/NewsfeedCommentMvcTest.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.newsfeed.config.WebSecurityConfig;
 import com.sparta.newsfeed.controller.CommentController;
 import com.sparta.newsfeed.controller.NewsfeedController;
-import com.sparta.newsfeed.dto.NewsfeedRequestDto;
+import com.sparta.newsfeed.dto.*;
 import com.sparta.newsfeed.entity.User;
 import com.sparta.newsfeed.entity.UserRoleEnum;
 import com.sparta.newsfeed.security.UserDetailsImpl;
@@ -27,7 +27,7 @@ import java.security.Principal;
 import java.time.LocalDateTime;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -113,6 +113,226 @@ class NewsfeedCommentMvcTest {
         // when - then
         mvc.perform(post("/api/newsfeed")
                         .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물 전체 조회")
+    void test2() throws Exception {
+        // given
+        this.mockUserSetup();
+        Long id = 1L;
+        String title = "게시물 제목";
+        String content = "게시물 내용";
+        NewsfeedResponseDto responseDto = new NewsfeedResponseDto(
+                id,
+                title,
+                content
+        );
+        String postInfo = objectMapper.writeValueAsString(responseDto);
+        // when - then
+        mvc.perform(get("/api/newsfeed")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물 단건 조회")
+    void test3() throws Exception {
+        // given
+        this.mockUserSetup();
+        Long id = 1L;
+        String title = "게시물 제목";
+        String content = "게시물 내용";
+        NewsfeedResponseDto responseDto = new NewsfeedResponseDto(
+                id,
+                title,
+                content
+        );
+        String postInfo = objectMapper.writeValueAsString(responseDto);
+        // when - then
+        mvc.perform(get("/api/newsfeed/1")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물 수정")
+    void test4() throws Exception {
+        // given
+        this.mockUserSetup();
+        Long id = 1L;
+        String title = "게시물 제목";
+        String content = "게시물 내용 수정";
+        NewsfeedResponseDto responseDto = new NewsfeedResponseDto(
+                id,
+                title,
+                content
+        );
+        String postInfo = objectMapper.writeValueAsString(responseDto);
+        // when - then
+        mvc.perform(put("/api/newsfeed/1")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물 삭제")
+    void test5() throws Exception {
+        // given
+        this.mockUserSetup();
+        Long id = 1L;
+        String title = "게시물 제목";
+        String content = "게시물 내용 수정";
+        NewsfeedResponseDto responseDto = new NewsfeedResponseDto(
+                id,
+                title,
+                content
+        );
+        String postInfo = objectMapper.writeValueAsString(responseDto);
+        // when - then
+        mvc.perform(delete("/api/newsfeed/1")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("게시물 10건 씩 페이지 조회")
+    void test6() throws Exception {
+        // given
+        this.mockUserSetup();
+        LocalDateTime startDate = LocalDateTime.now();
+        LocalDateTime endDate = LocalDateTime.now();
+        String sortBy = "writeDate";
+        PagingRequestDto requestDto = new PagingRequestDto(
+                startDate,
+                endDate,
+                sortBy
+        );
+        String postInfo = objectMapper.writeValueAsString(requestDto);
+        // when - then
+        mvc.perform(get("/api/newsfeed/page/1")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("댓글 생성")
+    void test7() throws Exception {
+        // given
+        this.mockUserSetup();
+        String username = "사용자명";
+        String comment = "댓글";
+        String nickname = "별명";
+
+        CommentCreateRequest requestDto = new CommentCreateRequest(
+                comment,
+                username,
+                nickname
+        );
+        String postInfo = objectMapper.writeValueAsString(requestDto);
+        // when - then
+        mvc.perform(post("/api/1/comments")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("댓글 조회")
+    void test8() throws Exception {
+        // given
+        this.mockUserSetup();
+        Long id = 1L;
+        String title = "제목";
+        String comment = "내용";
+        Long good_counting = 1L;
+
+        CommentResponse response = new CommentResponse(
+                id,
+                title,
+                comment,
+                good_counting
+        );
+        String postInfo = objectMapper.writeValueAsString(response);
+        // when - then
+        mvc.perform(get("/api/1/comments/1")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("댓글 수정")
+    void test9() throws Exception {
+        // given
+        this.mockUserSetup();
+        Long id = 1L;
+        String comment = "내용";
+
+        CommentUpdateRequest updateRequest = new CommentUpdateRequest(
+                id,
+                comment
+        );
+        String postInfo = objectMapper.writeValueAsString(updateRequest);
+        // when - then
+        mvc.perform(put("/api/1/comments/1")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("댓글 삭제")
+    void test10() throws Exception {
+        // given
+        this.mockUserSetup();
+
+        // when - then
+        mvc.perform(delete("/api/1/comments/1")
+                        //.content(postInfo)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                         .principal(mockPrincipal)
